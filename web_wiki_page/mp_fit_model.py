80c5ad5e9dfdf6cb2638d50048ac9f398bd2780b
dboyliao <qmalliao@gmail.com>
2017-08-06 21:34:51 +0800

    Update scripts

--- web_wiki_page/mp_fit_model.py
+++ web_wiki_page/mp_fit_model.py
@@ -5,6 +5,7 @@ import pickle
 from multiprocessing import Process
 from multiprocessing import cpu_count, current_process
 from multiprocessing import Queue, JoinableQueue
+from multiprocessing.queues import Empty
 import pandas as pd
 from tasks import fit_model
 
@@ -14,6 +15,11 @@ def worker(in_queue, out_queue):
     while True:
         try:
             task = in_queue.get_nowait()
+            if task is None:
+                print("[{}] Sentinal value detected, worker exit".format(pid),
+                      flush=True)
+                in_queue.task_done()
+                break
             lags_to_try, ts = task
             page, access = ts[0], ts[1:]
             print("[{}] Processing {}".format(pid, page), flush=True)
@@ -27,24 +33,26 @@ def worker(in_queue, out_queue):
                 results[num_lags] = result
             out_queue.put((page, results))
             in_queue.task_done()
-        except Exception:
+        except Empty:
+            # waiting for task
             pass
 
 
-def consumer(out_queue):
+def consumer(out_queue, out_fname):
     models = {}
     while True:
         try:
             record = out_queue.get_nowait()
             if record is None:
+                print("Sentinal value detected, consumer exit", flush=True)
                 break
             page, result = record
             models[page] = result
-        except Exception:
+        except Empty:
+            # waiting for record
             pass
-
     # Saving results
-    with open("models.pickle", "wb") as wf:
+    with open(out_fname, "wb") as wf:
         pickle.dump(models, wf)
 
 
@@ -55,11 +63,18 @@ if __name__ == "__main__":
                         help="number of processes",
                         default=cpu_count()*2,
                         type=int)
+    parser.add_argument("csv_file", help="input csv file")
+    parser.add_argument("-o", "--output-fname", help="output file name (pickle file)",
+                        dest="out_fname", default=None)
     args = vars(parser.parse_args())
     num_workers = args["num_workers"]
+    csv_file = args["csv_file"]
+    out_fname = args["out_fname"]
+    if out_fname is None:
+        out_fname = csv_file.split(".")[0] + ".pickle"
 
-    print("reading data")
-    train_data = pd.read_csv("./train_1.csv", engine='c')
+    print("reading data: {}".format(csv_file), flush=True)
+    train_data = pd.read_csv(csv_file, engine='c')
     train_data.fillna(0, inplace=True)
 
     in_queue = JoinableQueue()
@@ -69,8 +84,7 @@ if __name__ == "__main__":
         p.daemon = True
         p.start()
 
-    p_consume = Process(target=consumer, args=(out_queue,))
-    p_consume.daemon = True
+    p_consume = Process(target=consumer, args=(out_queue, out_fname,))
     p_consume.start()
 
     lags_to_try = [i for i in range(1, 9, 2)]
@@ -84,6 +98,9 @@ if __name__ == "__main__":
             print("data enqued: {:.2f}%".format(100.0*(index+1)/num_series),
                   flush=True)
     print("data enqued: 100.00%", flush=True)
+    for _ in range(num_workers):
+        in_queue.put(None)
+    print("Input queue setup done", flush=True)
     in_queue.close()
     in_queue.join()
     print("Input queue joined", flush=True)
